16fdad20a93f6b898d7c54c9b1f2e56fb01a2273
Akira Kawai <kawaiakira51@gmail.com>
2022-02-01 12:28:52 +0900

    added <> on tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,32 +6,34 @@ import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
  return (
-  <div className={styles.container}>
-   <Head>
-    <title>Welcome to this page!</title>
-    <link rel="icon" href="/favicon.ico" />
-   </Head>
+  <>
+   <div className={styles.container}>
+    <Head>
+     <title>Welcome to this page!</title>
+     <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-   <main>
-    <h1 className={styles.title}>
-     Read{' '}
-     <Link href="/posts/first-post">
-      <a>My First Page!</a>
-     </Link>
-    </h1>
-    <div className={styles.grid}>
-     <div className={styles.card}>
-      <Link href="/models">
-       <h3>Try Models &rarr;</h3>
+    <main>
+     <h1 className={styles.title}>
+      Read{' '}
+      <Link href="/posts/first-post">
+       <a>My First Page!</a>
       </Link>
-      <p>Find in-depth information about Next.js features and API.</p>
+     </h1>
+     <div className={styles.grid}>
+      <div className={styles.card}>
+       <Link href="/models">
+        <h3>Try Models &rarr;</h3>
+       </Link>
+       <p>Find in-depth information about Next.js features and API.</p>
+      </div>
      </div>
-    </div>
-   </main>
-   <footer>
-    Powered by <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-   </footer>
-  </div>
+    </main>
+    <footer>
+     Powered by <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+    </footer>
+   </div>
+  </>
  );
 };
 
--- pages/models/index.tsx
+++ pages/models/index.tsx
@@ -4,14 +4,15 @@ import styles from '../../styles/Home.module.css';
 
 export default function Models() {
  return (
-  //   <>
-  <div className={styles.grid}>
-   <div className={styles.card}>
-    <Link href="/models/digit-recognizer">
-     <h3>Try Digit Recognizer &rarr;</h3>
-    </Link>
+  <>
+   <div className={styles.grid}>
+    <div className={styles.card}>
+     <Link href="/models/digit-recognizer">
+      <h3>Try Digit Recognizer &rarr;</h3>
+     </Link>
+    </div>
+    <p>Find in-depth information about Next.js features and API.</p>
    </div>
-   <p>Find in-depth information about Next.js features and API.</p>
-  </div>
+  </>
  );
 }
