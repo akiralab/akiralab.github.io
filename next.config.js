c2cc642614221c8c89f2b5e776bc20172cb834f5
Akira Kawai <kawaiakira51@gmail.com>
2022-02-03 18:51:19 +0900

    image loader added

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.GITHUB_ACTIONS ? '/akiralab.github.io' : '',
   trailingSlash: true,
+  images: {
+    loader: 'imgix',
+    path: 'https://akiralab.github.io',
+  },  
 }
 
 module.exports = nextConfig
