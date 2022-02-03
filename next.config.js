273c543165924b58d913341efa143f54c8443764
Akira Kawai <kawaiakira51@gmail.com>
2022-02-03 18:04:32 +0900

    初期化

--- next.config.js
+++ next.config.js
@@ -1,40 +1,9 @@
 /** @type {import('next').NextConfig} */
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: process.env.GITHUB_ACTIONS ? '/akiralab.github.io' : '',
+  trailingSlash: true,
+}
 
-module.exports = {
- basePath: process.env.GITHUB_ACTIONS ? '/akiralab.github.io' : '',
- trailingSlash: true,
- reactStrictMode: true,
- distDir: 'build',
- webpack: (config, {}) => {
-  config.resolve.extensions.push('.ts', '.tsx');
-  config.resolve.fallback = { fs: false };
+module.exports = nextConfig
 
-  config.plugins.push(
-   new NodePolyfillPlugin(),
-   new CopyPlugin({
-    patterns: [
-     {
-      from: './node_modules/onnxruntime-web/dist/ort-wasm.wasm',
-      to: 'static/chunks/pages',
-     },
-     {
-      from: './node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
-      to: 'static/chunks/pages',
-     },
-     {
-      from: './model/',
-      to: 'static/chunks/pages',
-     },
-     {
-      from: './styles/',
-      to: 'static/chunks/pages',
-     },
-    ],
-   })
-  );
-
-  return config;
- },
-};
