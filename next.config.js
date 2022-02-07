b710e9a1f2fb4ca002f4810a981f62aee437667f
Akira Kawai <kawaiakira51@gmail.com>
2022-02-07 13:19:28 +0900

    next.config.js changed

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // basePath: process.env.GITHUB_ACTIONS ? '/akiralab.github.io' : '',
-  // trailingSlash: true,
+  basePath: process.env.GITHUB_ACTIONS ? '/akiralab.github.io/akiralab.github.io' : '',
+  trailingSlash: true,
   images: {
     loader: 'imgix',
     path: 'https://akiralab.github.io',
