150288c92d8d382dec287d2020ed127c32ece3bc
akiralab <akiralab@users.noreply.github.com>
2022-04-06 13:24:34 +0000

    deploy: 923f853e2ed94c01df29efe5acd2dd83a31438db

--- _next/static/tkEd3vPLQHNiwFYGHfG8k/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-31aeea06b129ae22.js","static/css/95594788432a01c3.css","static/chunks/pages/index-6c4f4aeb48954dbe.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
