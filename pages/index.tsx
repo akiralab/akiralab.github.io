9ddc9aac5bbd7e504c1bc66ee8cbae7f89470df1
Akira Kawai <kawaiakira51@gmail.com>
2022-04-16 23:33:55 +0900

    playground page

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,12 @@ const Home: NextPage = () => {
       <p>コンテンツ公開中</p>
      </div>
     </Link>
+    <Link href="/playground">
+     <div className={styles.card}>
+      <h2>React Playground</h2>
+      <p>コンテンツ公開中</p>
+     </div>
+    </Link>
     <Link href="/digit-recognizer">
      <div className={styles.card}>
       <h2>Digit Recognizer</h2>
