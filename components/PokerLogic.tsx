ff35e810228d16a8cae2ac014487e934e4e2a39b
Akira Kawai <kawaiakira51@gmail.com>
2022-09-15 13:33:13 +0900

    バグ修正

--- components/PokerLogic.tsx
+++ components/PokerLogic.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import { useRef, useState } from "react";
 
 const PokerLogic = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const getTable = () => {
-    const canvas = canvasRef.current;
-    const ctx = canvas!.getContext("2d");
-    ctx!.drawImage(image, 0, 0, props.width, props.height);
-  };
-
+  //   const canvasRef = useRef<HTMLCanvasElement>(null);
+  //   const getTable = () => {
+  //     const canvas = canvasRef.current;
+  //     const ctx = canvas!.getContext("2d");
+  //     ctx!.drawImage(image, 0, 0, props.width, props.height);
+  //   };
   return (
     <>
       <div>Poker Logic</div>
-      <canvas ref={canvasRef} />
+      {/* <canvas ref={canvasRef} /> */}
     </>
   );
 };
