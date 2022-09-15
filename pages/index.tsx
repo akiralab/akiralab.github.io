8650a6b0b4330e2cca979a0833b004be9571da1d
Akira Kawai <kawaiakira51@gmail.com>
2022-09-15 13:24:16 +0900

    notion, zennのリンク追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,18 @@ const Home: NextPage = () => {
             <p>コンテンツ公開中</p>
           </div>
         </Link>
+        <Link href="https://blog-ochre-gamma.vercel.app/" passHref>
+          <div className={styles.card}>
+            <h2>Notion with Next.js</h2>
+            <p>コンテンツ公開中</p>
+          </div>
+        </Link>
+        <Link href="https://zenn.dev/chankawa" passHref>
+          <div className={styles.card}>
+            <h2>Zenn articles</h2>
+            <p>コンテンツ公開中</p>
+          </div>
+        </Link>
         <Link href="/digit-recognizer" passHref>
           <div className={styles.card}>
             <h2>Digit Recognizer</h2>
@@ -29,24 +41,6 @@ const Home: NextPage = () => {
         <Link href="/simple-poker" passHref>
           <div className={styles.card}>
             <h2>原始ポーカー</h2>
-            <p>コンテンツ準備中</p>
-          </div>
-        </Link>{" "}
-        <Link href="/notion" passHref>
-          <div className={styles.card}>
-            <h2>Notion with Next.js</h2>
-            <p>コンテンツ準備中</p>
-          </div>
-        </Link>
-        <Link href="/blogs" passHref>
-          <div className={styles.card}>
-            <h2>ML blogs</h2>
-            <p>コンテンツ準備中</p>
-          </div>
-        </Link>
-        <Link href="/zenn" passHref>
-          <div className={styles.card}>
-            <h2>Zenn articles</h2>
             <p>コンテンツ準備中</p>
           </div>
         </Link>
